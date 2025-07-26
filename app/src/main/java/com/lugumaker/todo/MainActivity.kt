bed37bf9663c87717c49f684fe07ba2089b09718
Yo-code <133862919+Yo-codeback@users.noreply.github.com>
2025-07-26 15:55:59 +0800

    在 MainActivity 中引入 LaunchedEffect，以支援組件的生命週期管理

--- app/src/main/java/com/lugumaker/todo/MainActivity.kt
+++ app/src/main/java/com/lugumaker/todo/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.lugumaker.todo.ui.ToDoScreen
